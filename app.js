3e701c8153a4bd83b760d817306b7faaff8877c5
jimmy surprenant <jimmy.surprenant@espritslibres.ca>
2018-05-29 12:58:03 -0400

    Fix little bugs in order to have our first HELLO WORLD

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import routes from './routes';
 // Express app setup
 const app = express();
 
+app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('*', (req, res) => {
-  res.render('index');
+  res.render('index', {title: 'Hey there,', message : 'Hello World'});
 });
 
 export default router;
